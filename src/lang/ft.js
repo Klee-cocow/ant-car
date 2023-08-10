653f4671394ca1a43e0f1084b91d61a6170181a5
咏鹅 <cocoww595@gmail.com>
2023-08-10 17:07:24 +0800

    add language

--- src/lang/ft.js
+++ src/lang/ft.js
@@ -1,6 +1,23 @@
 const ft = {
-  Home: "Maison",
+  Home: "Accueil",
   Content: "Contenu",
   Welcome: "Bienvenue",
+  ContactUs: "Contactez-nous",
+  Telephone: "Téléphone",
+  Contact: "Contact",
+  Blogs: "Articles",
+  ProductOverview: "Aperçu du produit",
+  readmore: "LIRE LA SUITE",
+  reason:
+    "Nous avons une excellente qualité de produit, une technologie et un équipement innovants, une chaîne d'approvisionnement et une livraison fiables, un excellent service client, des initiatives de développement durable et environnementales, des stratégies de tarification raisonnables, une réputation et une notoriété de marque, des solutions personnalisées et des relations de coopération à long terme.",
+  productTitle: "POURQUOI NOUS CHOISIR ?",
+  Copyright: "Droit d'auteur",
+  Help: "Aide",
+  Help1: "",
+  Vehicle: "Véhicule",
+  Telephone: "Téléphone",
+  Mobilephone: "Téléphone portable (WhatsApp/WeChat) n°",
+  Email: "E-mail :",
+  Address: "Adresse : Maison du grand-père dans le jardin fleuri",
 };
 export default ft;
--- src/lang/pt.js
+++ src/lang/pt.js
@@ -1,6 +1,23 @@
 const pt = {
-  Home: "Casa",
+  Home: "Página Inicial",
   Content: "Conteúdo",
   Welcome: "Bem-vindo",
+  ContactUs: "Contate-nos",
+  Telephone: "Telefone",
+  Contact: "Contato",
+  Blogs: "Blogs",
+  ProductOverview: "Visão Geral do Produto",
+  readmore: "LER MAIS",
+  reason:
+    "Temos excelente qualidade de produto, tecnologia e equipamento inovadores, cadeia de suprimentos e entrega confiáveis, excelente atendimento ao cliente, iniciativas sustentáveis e ambientais, estratégias de preços razoáveis, reputação e reconhecimento de marca, soluções personalizadas e relacionamentos de cooperação de longo prazo.",
+  productTitle: "POR QUE NOS ESCOLHER?",
+  Copyright: "Direitos Autorais",
+  Help: "Ajuda",
+  Help1: "",
+  Vehicle: "Veículo",
+  Telephone: "Telefone",
+  Mobilephone: "Telefone Celular (WhatsApp/WeChat) Nº",
+  Email: "E-mail:",
+  Address: "Endereço: Casa do avô no jardim das flores",
 };
 export default pt;
